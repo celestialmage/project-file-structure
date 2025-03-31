315e95530ebb1701794eb4e6b21ad5affb49e282
Max <imax.mr@gmail.com>
2025-03-31 14:21:49 -0700

    reorganized file structure

--- tests/test_division.py
+++ tests/test_division.py
@@ -1,7 +1,6 @@
-import division
+import calculator.division as division
 
 def test_division():
     assert division.perform_operation(10, 5) == 2
     assert division.perform_operation(1, 1) == 1
     assert division.perform_operation(100, 2) == 50
-
